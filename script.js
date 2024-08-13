f6a495f9a50f4eaef7923301c51a652542e6d34a
idrmdhni <indrmdhni@gmail.com>
2024-08-13 20:18:03 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const nameInput = document.getElementById("name-input");
+const nameSubmit = document.getElementById("name-submit");
+
+function randomKhodam() {
+  const listKhodam = [
+    "Rehan Wangsaf",
+    "Bagas Dribble",
+    "Sandal Jepit",
+    "Gedebog Pisang",
+    "Kepiting Malang",
+    "Ikan Berkepala Lele",
+    "Laba-laba Sunda",
+    "Macan Cisewu",
+    "Ferdi Kopling",
+    "Velg Mio",
+    "Oli Samping",
+    "Sigit Rendang",
+    "Bakwan Jagung",
+    "Pecel Lele",
+    "Nasi Kucing",
+    "Kue Nastar",
+  ];
+  const indexKhodam = Math.round(Math.random() * (listKhodam.length - 1));
+  console.log(indexKhodam);
+  return listKhodam[indexKhodam];
+}
+
+nameSubmit.addEventListener("click", () => {
+  const name = document.querySelector(".result p");
+  const khodam = document.querySelector(".result span");
+
+  name.innerHTML = `${nameInput.value}, khodam yang ada di dalam diri kamu adalah`;
+  khodam.innerHTML = randomKhodam();
+});
